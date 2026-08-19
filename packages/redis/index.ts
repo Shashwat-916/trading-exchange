40923b06b3146ad1f1da20414fbed5ee5aff4c7f
Shashwat-916 <shashwatdev4009@gmail.com>
2026-08-19 19:32:47 +0530

    added redis singleton to use it auth + in future usage { rate-limiter orders etc  }

--- packages/redis/index.ts
+++ packages/redis/index.ts
@@ -1,1 +1,32 @@
-import redis from 'redis'
+import { createClient, type RedisClientType } from 'redis'
+
+
+export class RedisManager {
+
+    private client: RedisClientType;
+    private static instance: RedisManager
+
+    private constructor() {
+        this.client = createClient()
+        this.client.on('error', (err) => { console.error("Redis Client Error") })
+        this.client.on('connect', () => { console.log("Redis connected Successfully ") })
+        this.client.connect().catch(console.error)
+    }
+
+    public static getInstance(): RedisManager {
+        if (!this.instance) {
+            this.instance = new RedisManager()
+        }
+        return this.instance
+    }
+
+    public getClient(): RedisClientType {
+        return this.client
+    }
+
+}
+
+
+export const redisInstance = RedisManager.getInstance()
+export const redis = redisInstance.getClient()
+
